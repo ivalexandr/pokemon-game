64019f73eaf4e7a2de531941499973bb2799200d
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-01-16 18:29:19 +0300

    complete homework-1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,33 @@
-import './App.css'
+import { Header } from '../Header/Header'
+import { Layout } from '../Layout/Layout'
+import { Footer } from '../Footer/Footer'
+import bg from '../../assets/img/bg.jpg'
+
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <Header 
+      title = "This is title"
+      descr = "This is Description!"
+      />
+      <Layout
+      title = "This is title layout 1"
+      descr = "This is descr layout 1"
+      urlBg = {bg}
+      />
+      <Layout 
+      title = "This is title layout 2"
+      descr = "This is descr layout 2"
+      colorBg = "#e2e2e2"
+      />
+      <Layout
+      title = "This is title layout 3"
+      descr = "This is descr layout 3"
+      urlBg = {bg}
+      />
+      <Footer />
+    </>
   )
 }
 
