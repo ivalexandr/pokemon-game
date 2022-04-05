1dda85884c9d7b37f1a1a4ef86df313a8e822d6c
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-04-05 20:40:16 +0300

    added routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { LayoutPage } from "../LayoutPage"
 import { HomePage } from "../../routes/HomePage"
 import { GamePage } from '../../routes/GamePage'
+import { AboutPage } from "../../routes/AboutPage"
+import { Page404 } from "../../routes/404"
 
 const App = () => {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LayoutPage />}>
           <Route path="/" element={<HomePage />}/>
-          <Route path="game" element={<GamePage />}/>
+          <Route path="game" element={<GamePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<AboutPage />} />
         </Route>
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
   )
