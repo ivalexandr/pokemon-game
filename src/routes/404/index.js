146b42197c412ea08ac09a075637a5e1f8589fe1
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-04-27 15:56:02 +0300

    refactoring page 404

--- src/routes/404/index.js
+++ src/routes/404/index.js
@@ -1,6 +1,12 @@
+import s from './style.module.css'
+
 const Page404 = () => {
   return (
-    <h1>Page not found!!!</h1>
+    <div className={s.wrapper}>
+        <div className={s.error}>Упс, кажется произошла ошибка: 404</div>
+        <h1 className={s.title}>Страница не найдена!</h1>
+        <div className={s.smile}> :( </div>
+    </div>
   )
 }
 
